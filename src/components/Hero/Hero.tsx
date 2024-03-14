b46b964f705189a90c424510d54e376b3bb05232
m-muiz <mmuizsiddiqui@gmail.com>
2024-03-14 23:58:27 +0500

    add hero section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import a from "../../assets/hero_main.webp"
+
+const Hero = () => {
+  return (
+    <div>
+        <div className=""></div>
+        <div className="">
+            <img src={a} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
