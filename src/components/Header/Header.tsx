876526be05d9852a020da4bd4f63c599d6cd36ab
m-muiz <mmuizsiddiqui@gmail.com>
2024-03-16 23:48:09 +0500

    complete hero section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,17 +4,17 @@ import h from "../../assets/logo.svg"
 
 const Header = () => {
     return (
-        <div className="m-4 p-6 gray_background flex items-center justify-between rounded-lg fixed left-0 right-0">
+        <div className="m-4 p-6 gray_background flex items-center justify-between rounded-lg fixed left-0 right-0 max-w-[1800px] mx-auto">
             <div className="logo">
                 <img src={h} alt="logo" />
             </div>
 
             <div>
                 <ul className="flex items-center gap-3">
-                    <li className="cursor-pointer">Offer</li>
-                    <li className="flex gap-2 items-center cursor-pointer">Flex <IoMdHeart className="gredient_color" /> atmosphere</li>
-                    <li className="cursor-pointer">Blog</li>
-                    <li className="cursor-pointer">Help</li>
+                    <li className="cursor-pointer hover p-2">Offer</li>
+                    <li className="flex gap-2 items-center cursor-pointer hover p-2">Flex <IoMdHeart className="gredient_color" /> atmosphere</li>
+                    <li className="cursor-pointer hover p-2">Blog</li>
+                    <li className="cursor-pointer hover p-2">Help</li>
                 </ul>
             </div>
             <div>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,19 +1,19 @@
-import a from "../../assets/hero_main.webp";
+import a from "../../assets/ds.webp";
 import b from "../../assets/scan.png"
 
 const Hero = () => {
     return (
-        <div className="flex items-center justify-between gap-4">
+        <div className="flex items-center justify-between gap-8  max-w-6xl mx-auto">
 
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-7">
                 <h1 className="text-8xl text-white font-semibold">Orange <br /> Flex</h1>
                 <p className="secondary_text">
                     Not prepaid, not subscription.<br /> Subscription! Orange Flex is enough.
                 </p>
-                <img src={b} alt="" />
+                <img className="w-2/3" src={b} alt="" />
             </div>
-            <div className="h-full w-1/2 mr-6">
-                <img src={a} alt="" />
+            <div className="h-full">
+                <img src={a} alt="Hero_Image" className="h-full w-full object-cover" />
             </div>
         </div>
     )
